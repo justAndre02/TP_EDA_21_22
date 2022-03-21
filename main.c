812a60f07cd863acd8f9253567d2690c9464d5b9
André Freitas <andregabriel@live.com.pt>
2022-03-21 10:15:54 +0000

    Criação do projeto

---
+++ main.c
@@ -0,0 +1,19 @@
+/**
+ * @file main.c
+ * @author André (a21112@alunos.ipca.pt)
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-21
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct {
+    int Nmaquinas;
+    int Noperacoes;
+} Maquina;
